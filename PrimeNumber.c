314e3829835c37180436b2d5477bc14f481635ff
Maha6468 <motherma7633@gmail.com>
2025-03-15 14:18:33 +0600

    Check prime number

---
+++ PrimeNumber.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num, i;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num < 2) {
+        printf("%d is Not a Prime Number.\n", num);
+        return 0;
+    }
+    for (i = 2; i < num; i++) {
+        if (num % i == 0) {
+            printf("%d is Not a Prime Number.\n", num);
+            return 0;
+        }
+    }
+    printf("%d is a Prime Number.\n", num);
+
+    return 0;
+}
