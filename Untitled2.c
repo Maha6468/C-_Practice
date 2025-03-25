1248f3721753f766444944751d3b7b4c1f6c07b2
Maha6468 <motherma7633@gmail.com>
2025-03-25 12:36:21 +0600

    basic

--- Untitled2.c
+++ Untitled2.c
@@ -1,12 +1,1 @@
-#include<stdio.h>
-int main()
-{
-char ch;
-//printf("Enter a value:");
-ch=getchar();
-//printf("You entered:");
-putchar(ch);
 
-
-
-}
---
+++ dowhileloop.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+main()
+{
+    int i=1;
+    do
+    {
+        printf("%d\n",i); //1
+        i++;   //2
+    }
+    while(i<=10);
+}
